ab779863aa2063d134ace008f89f34db683a08a7
jinaa11 <jinaa11@gmail.com>
2023-01-25 17:36:04 +0900

    module.css

--- kdt_shop/src/components/layout/Header.jsx
+++ kdt_shop/src/components/layout/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import styles from './Header.module.css'
 
 function Header() {
    return (
       <header>
-         <h1>SSG.com</h1>
+         <h1 className={styles.logo}>SSG.com</h1>
          <nav>
             <ul>
                <li><Link to='/'>Main</Link></li>
