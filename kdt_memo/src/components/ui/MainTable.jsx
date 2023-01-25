4ff60a0a8f75b08a5ad74ed1777a63f904976215
jinaa11 <jinaa11@gmail.com>
2023-01-25 14:02:04 +0900

    fetch update

--- kdt_memo/src/components/ui/MainTable.jsx
+++ kdt_memo/src/components/ui/MainTable.jsx
@@ -6,19 +6,22 @@ import WordList from './WordList';
 
 
 function MainTable() {
+  // State 추가
   const [data, setData] = useState();
-  
-  useEffect(()=> {
+  const [isCheck, setIsCheck] = useState(false);
+
+  // 실행되면 데이터 호출, isCheck가 바뀌면 호출
+  useEffect(() => {
     fetch('http://localhost:3001/words')
-    // 제이슨으로 풀어내고
-    .then(res=>res.json())
-    // data에 옮겨 담음
-    .then(data=> {
-      console.log(data);
-      setData(data)
-    })
-    .catch(err=>console.log(err));
-  }, []);
+      // 제이슨으로 풀어내고
+      .then(res => res.json())
+      // data에 옮겨 담음
+      .then(data => {
+        console.log(data);
+        setData(data)
+      })
+      .catch(err => console.log(err));
+  }, [isCheck]);
 
   return (
     <Table striped bordered hover>
@@ -33,9 +36,14 @@ function MainTable() {
       </thead>
       <tbody>
         {
-          // 데이터가 있으면 실행
+          // 데이터가 있으면 실행하고
+          // WordList로 props 전달
+          // 없으면 동작 X
           data && data.map(word => (
-            <WordList key={word.id} word={word} />
+            <WordList key={word.id}
+              word={word}
+              isCheck={isCheck}
+              setIsCheck={setIsCheck} />
           ))
         }
       </tbody>
--- kdt_memo/src/components/ui/WordList.jsx
+++ kdt_memo/src/components/ui/WordList.jsx
@@ -3,20 +3,42 @@ import { useState } from 'react';
 import Forms from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-function WordList({ word }) {
+// 상태 바뀐거 체크하고 싶음 (isCheck, setIsCheck)
+function WordList({ word, isCheck, setIsCheck }) {
    const [isView, setIsView] = useState(false);
 
    const handleView = () => {
       setIsView(!isView);
    }
 
+   const handleCheck = () => {
+      fetch(`http://localhost:3001/words/${word.id}`, {
+         method: 'PUT',
+         headers: {
+            'Content-Type': 'application/json'
+         },
+         body: JSON.stringify({
+            // 기존 값에 더해서 바뀐 부분만 체크
+            ...word,
+            isDone: !word.isDone
+         })
+      }).then(res => {
+         console.log(res);
+         if (res.ok) {
+            setIsCheck(!isCheck);
+         }
+      })
+   }
+
    return (
       <tr key={word.id}>
          <td>{word.id}</td>
          <td>
-            <Forms.Check type="checkbox"
-               disabled={word.isDone}
+            <Forms.Check
+               type="checkbox"
                defaultChecked={word.isDone}
+               disabled={word.isDone}
+               onChange={handleCheck}
             />
          </td>
          <td>{word.eng}</td>
