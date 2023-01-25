92abbc51ad39214fdeec7e368f46a8192089dd1b
jinaa11 <jinaa11@gmail.com>
2023-01-25 13:16:57 +0900

    fetch

--- kdt_memo/src/components/ui/MainTable.jsx
+++ kdt_memo/src/components/ui/MainTable.jsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
-import data from '../../db/data.json'
+// import data from '../../db/data.json'
 import WordList from './WordList';
 
 
 function MainTable() {
+  const [data, setData] = useState();
+  
+  useEffect(()=> {
+    fetch('http://localhost:3001/words')
+    // 제이슨으로 풀어내고
+    .then(res=>res.json())
+    // data에 옮겨 담음
+    .then(data=>setData(data))
+    .catch(err=>console.log(err));
+  }, []);
+
   return (
     <Table striped bordered hover>
       <thead>
