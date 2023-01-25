26dc9829dfe3dad8587e9a369afc8cb4259f539e
jinaa11 <jinaa11@gmail.com>
2023-01-25 10:53:31 +0900

    added table and button

---
+++ kdt_memo/src/components/ui/MainTable.jsx
@@ -0,0 +1,35 @@
+import Table from 'react-bootstrap/Table';
+import Forms from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+function MainTable() {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>check</th>
+          <th>english</th>
+          <th>korean</th>
+          <th>controller</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>
+            <Forms.Check type="checkbox" />
+          </td>
+          <td>apple</td>
+          <td>사과</td>
+          <td>
+            <Button variant="primary">답 보기</Button>{' '}
+            <Button variant="secondary">삭제</Button>{' '}
+          </td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+}
+
+export default MainTable;
