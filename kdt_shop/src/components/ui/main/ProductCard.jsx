58bc95ddce0af6927b62a3981a3b3945f2c53e6d
jinaa11 <jinaa11@gmail.com>
2023-01-26 15:45:57 +0900

    add cart btn

--- kdt_shop/src/components/ui/main/ProductCard.jsx
+++ kdt_shop/src/components/ui/main/ProductCard.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import style from './ProductCard.module.css';
 
-function ProductCard({product}) {
+function ProductCard({ product }) {
    return (
       <>
          <div className={style.productCard}>
-            <img src={product.thumbnail} alt="" />
-            <p>{product.title}</p>
-            <p>{product.description}</p>
-            <p>{product.price}</p>
+            <Link to={`/product-detail/${product.id}`}>
+               <img src={product.thumbnail} alt={product.description} />
+               <p>{product.title}</p>
+               <p>{product.description}</p>
+               <p>{product.price}</p>
+            </Link>
+
+            <div className={style.cartBtn}> ADD CART</div>
          </div>
       </>
    );
