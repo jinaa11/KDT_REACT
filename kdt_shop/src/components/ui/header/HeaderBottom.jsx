e6130203e3fbd46bfd9a497ca9ef4b72e6d45019
jinaa11 <jinaa11@gmail.com>
2023-01-26 10:40:27 +0900

    useLocation

--- kdt_shop/src/components/ui/header/HeaderBottom.jsx
+++ kdt_shop/src/components/ui/header/HeaderBottom.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import style from './HeaderBottom.module.css'
 import { gnbMenu } from '../../../data/gnbMenu';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function HeaderBottom() {
+
+   const location = useLocation();
+
    return (
       <nav className={style.gnb}>
          <ul>
             {
                gnbMenu.map(menu => (
-                  <li key={menu.id}><Link to ={menu.link}>{menu.name}</Link></li>
+                  <li key={menu.id} className={menu.link === location.pathname ? `${style.active}` : ''}>
+                     <Link to ={menu.link}>{menu.name}</Link></li>
                ))
             }
          </ul>
